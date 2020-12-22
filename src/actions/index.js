d4f341600f745f4d8212bbf95bff26cc5deb8fa3
ssjuampi23 <sspablo23@hotmail.com>
2020-12-22 08:57:18 -0600

    Hook up Redux Thunk into our Redux Store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,29 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-  // Because Redux Thunk allows to return a function
-  return function(dispatch, getState){
-      // dispatch => same Redux dispatch function
-      // getState => same Redux getState function to get the state of the Redux store
-      const promise = jsonPlaceholder.get('/posts');
+// Refactor
+// 1. remove function keyword and use arrow function
+// 2. remove getState param because we are not using it
+// 3. remove () around dispatch
+// 4. remove return statement and brackets since we are returning 1 single thing
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
 
-      return {
-          type: 'FETCH_POSTS',
-          payload: promise
-      };
-  }
+  dispatch({ type: 'FETCH_POSTS', payload: response});
+};
+
+export const fetchPostsBeforeRefactor = () => {
+    // Because Redux Thunk allows to return a function
+    return async function(dispatch, getState){
+        // dispatch => same Redux dispatch function
+        // getState => same Redux getState function to get the state of the Redux store
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({ type: 'FETCH_POSTS', payload: response});
+    };
+};
+
+export const selectPost = () => {
+    return {
+        type: 'SELECT_POST'
+    }
 };
